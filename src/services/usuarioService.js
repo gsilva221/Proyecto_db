c696fbafcbc9cf7c6b4ef33b874d61d78ac70681
Kizhaw <priv.matiasmayea@gmail.com>
2026-06-28 20:15:36 -0400

    testing front

--- src/services/usuarioService.js
+++ src/services/usuarioService.js
@@ -2,32 +2,32 @@ import api from "./api";
 
 const usuarioService = {
     obtenerUsuarios: async () => {
-        const response = await api.get("/usuario");
+        const response = await api.get("/usuarios");
         return response.data;
     },
 
     obtenerUsuario: async (id) => {
-        const response = await api.get(`/usuario/${id}`);
+        const response = await api.get(`/usuarios/${id}`);
         return response.data;
     },
 
     crearUsuario: async (usuario) => {
-        const response = await api.post("/usuario", usuario);
+        const response = await api.post("/usuarios", usuario);
         return response.data;
     },
 
     actualizarUsuario: async (id, usuario) => {
-        const response = await api.put(`/usuario/${id}`, usuario);
+        const response = await api.put(`/usuarios/${id}`, usuario);
         return response.data;
     },
 
     eliminarUsuario: async (id) => {
-        const response = await api.delete(`/usuario/${id}`);
+        const response = await api.delete(`/usuarios/${id}`);
         return response.data;
     },
 
     login: async (credenciales) => {
-        const response = await api.post("/usuario/login", credenciales);
+        const response = await api.post("/usuarios/login", credenciales);
         return response.data;
     }
 };
